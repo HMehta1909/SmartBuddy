c0e78f33458c1e988215e19b1e6d20de85a40372
Himanshu <81746961+HMehta1909@users.noreply.github.com>
2023-08-21 17:13:22 +0530

    final touch

--- src/app/remove-resources/remove-resources.component.ts
+++ src/app/remove-resources/remove-resources.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { removeService } from './removeService.service';
 import { DataService } from '../register-resource/data.service';
+import { switchMap, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-remove-resources',
   templateUrl: './remove-resources.component.html',
@@ -15,42 +16,59 @@ export class RemoveResourcesComponent {
 [x: string]: any;
 
   constructor(private router:Router,private removeService: removeService ,private http: HttpClient, private route:ActivatedRoute, private dataService:DataService) {
-    // console.log("Nomination cons",this.nomination);
 
 
   }
   tableData:any;
-
+  employeeId: string = '';
   inputValue: string = '';
 
+  // del(id: any) {
+
+  //   this.dataService.getUser().subscribe((response: any[]) => {
+  //     this.tableData = response.filter(item => item.empId === id );
+  //   });
+  //   console.log(' ID:', this.tableData[0].id);
+  //   console.log('Deleting item with ID:', id);
+    
+  //   this.removeService.deleteResource(this.tableData[0].id).subscribe(
+  //     () => {
+  //       console.log('Item with ID', id, 'deleted successfully');
+  //       alert('Resource deleted successfully');
+  //     },
+  //     (error: any) => {
+  //       console.error('Error deleting item with ID', id, ':', error);
+  //       alert('No such Resource found please check ID');
+  //     }
+  //   );
+  // }
+
   del(id: any) {
-    // Implement your delete logic here
-    console.log('Deleting item with ID:', id);
-    this.removeService.deleteResource(id).subscribe(
-      () => {
-        console.log('Item with ID', id, 'deleted successfully');
-        alert('Resource deleted successfully');
-      },
-      (error: any) => {
-        console.error('Error deleting item with ID', id, ':', error);
-        alert('No such Resource found please check ID');
-      }
-    );
-  }
-
-  onButtonClick(){
-
-
     this.dataService.getUser().subscribe((response: any[]) => {
-
-      this.tableData = response.filter(item => item.empId === this.inputValue );
-
-      console.log(this.tableData);
-
+      const matchingItems = response.filter(item => item.empId === id);
+  
+      if (matchingItems.length === 0) {
+        console.warn('No resource found with ID:', id);
+        alert('No such resource found. Please check the ID.');
+        return;
+      }
+  
+      const resourceId = matchingItems[0].id;
+      console.log('Deleting item with ID:', resourceId);
+  
+      this.removeService.deleteResource(resourceId).subscribe(
+        () => {
+          console.log('Item with ID', resourceId, 'deleted successfully');
+          alert('Resource deleted successfully');
+        },
+        (error: any) => {
+          console.error('Error deleting item with ID', resourceId, ':', error);
+          alert('Error deleting resource. Please try again later.');
+        }
+      );
     });
-
-}
-
+  }
+  
 
   logout(){
     this.router.navigate(['']);
